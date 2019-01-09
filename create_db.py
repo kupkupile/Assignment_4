325868d486546cca305f84b351483c9ba32cef8b
Amit <kapami@post.bgu.ac.il>
2019-01-09 19:54:38 +0200

    Merge branch 'master' of https://github.com/kupkupile/Assignment_4
    # Conflicts:
    #	create_db.py

--- create_db.py
+++ create_db.py
@@ -9,23 +9,45 @@ def main():
     with dbcon:
         cursor = dbcon.cursor()
         if not databaseexisted:
-            cursor.execute("""CREATE TABLE students(grade TEXT PRIMARY KEY, count INTEGER NOT NULL);""")
-            cursor.execute("""CREATE TABLE courses(id INTEGER PRIMARY KEY, course_name TEXT NOT NULL ,student TEXT NOT NULL, number_of_students INTEGER NOT NULL, class_id INTEGER REFERENCES classrooms(id) ,course_length INTEGER NOT NULL);""")
-            cursor.execute("""CREATE TABLE classrooms(id INTEGER PRIMARY KEY ,location TEXT NOT NULL ,current_course_id INTEGER NOT NULL, current_course_time_left INTEGER NOT NULL);""")
+            cursor.execute("CREATE TABLE students(grade TEXT PRIMARY KEY, count INTEGER NOT NULL);")
+            cursor.execute("CREATE TABLE courses(id INTEGER PRIMARY KEY, course_name TEXT NOT NULL ,student TEXT NOT NULL, number_of_students INTEGER NOT NULL, class_id INTEGER REFERENCES classrooms(id) ,course_length INTEGER NOT NULL);")
+            cursor.execute("CREATE TABLE classrooms(id INTEGER PRIMARY KEY ,location TEXT NOT NULL ,current_course_id INTEGER NOT NULL, current_course_time_left INTEGER NOT NULL);")
 
-    inputfilename = sys.args[1]
-    with open(inputfilename) as inputfile:
-         for line in inputfile:
-             line = line.strip()
-             list_Of_Line = line.split(',')
-             if list_Of_Line[0] == 'C':
-                 cursor.execute("INSERT INTO courses(id,course_name,student,number_of_students,class_id,classrooms) VALUES(?,?,?,?,?,?)", (list_Of_Line[1].strip(), list_Of_Line[2].strip(),list_Of_Line[3].strip(),list_Of_Line[4].strip(),list_Of_Line[5].strip(),list_Of_Line[6].strip()))
+            inputfilename = sys.argv[1]
+            with open(inputfilename) as inputfile:
+                for line in inputfile:
+                    line = line.strip()
+                    list_Of_Line = line.split(',')
+                    if list_Of_Line[0] == 'C':
+                        cursor.execute("INSERT INTO courses(id,course_name,student,number_of_students,class_id,course_length) VALUES(?,?,?,?,?,?)", (list_Of_Line[1].strip(), list_Of_Line[2].strip(),list_Of_Line[3].strip(),list_Of_Line[4].strip(),list_Of_Line[5].strip(),list_Of_Line[6].strip()))
+                        dbcon.commit()
+                    if list_Of_Line[0] == 'S':
+                        cursor.execute("INSERT INTO students(grade,count) VALUES(?,?)",(list_Of_Line[1].strip(),list_Of_Line[2].strip()))
+                        dbcon.commit()
+                    if list_Of_Line[0] == 'R':
+                        cursor.execute("INSERT INTO classrooms(id,location,current_course_id,current_course_time_left) VALUES(?,?,0,0)", (list_Of_Line[1].strip(), list_Of_Line[2].strip()))
+                        dbcon.commit()
+            printAllTables()
 
-             if list_Of_Line[0] == 'S':
-                cursor.execute("INSERT INTO students(grade,count) VALUES(?,?)",(list_Of_Line[1].strip(),list_Of_Line[2].strip()))
-
-             if list_Of_Line[0] == 'R':
-                 cursor.execute("INSERT INTO students(id,location,current_course_id,current_course_time_left) VALUES(?,?,0,0)", (list_Of_Line[1], list_Of_Line[2]))
+def printAllTables():
+    dbcon = sqlite3.connect('schedule.db')
+    cursor = dbcon.cursor()
+    cursor.execute("SELECT * FROM courses")
+    list_of_all_courses = cursor.fetchall()
+    print("courses")
+    for course in list_of_all_courses:
+        print(course)
+    cursor.execute("SELECT * FROM classrooms")
+    list_of_all_classrooms = cursor.fetchall()
+    print("classrooms")
+    for classroom in list_of_all_classrooms:
+        print(classroom)
+    cursor.execute("SELECT * FROM students")
+    list_of_all_students = cursor.fetchall()
+    print("students")
+    for student in list_of_all_students:
+        print(student)
 
 if __name__ == '__main__':
     main()
+
--- schedule.py
+++ schedule.py
@@ -2,35 +2,84 @@ import sys
 import sqlite3
 import os
 
+def printAllTables():
+    dbcon = sqlite3.connect('schedule.db')
+    cursor = dbcon.cursor()
+    cursor.execute("SELECT * FROM courses")
+    list_of_all_courses = cursor.fetchall()
+    print("courses")
+    for course in list_of_all_courses:
+        print(course)
+    cursor.execute("SELECT * FROM classrooms")
+    list_of_all_classrooms = cursor.fetchall()
+    print("classrooms")
+    for classroom in list_of_all_classrooms:
+        print(classroom)
+    cursor.execute("SELECT * FROM students")
+    list_of_all_students = cursor.fetchall()
+    print("students")
+    for student in list_of_all_students:
+        print(student)
+
 def main():
     databaseexisted = os.path.isfile('schedule.db')
     dbcon = sqlite3.connect('schedule.db')
+    number_of_iteration = 0;
+    if check_if_table_is_empty():
+        printAllTables()
     while databaseexisted and not check_if_table_is_empty():
-        #check if a course which is in a class room is finished
         cursor = dbcon.cursor()
-        cursor.execute("SELECT * FROM classrooms WHERE current_course_id <> 0")
-        list_of_all_occupied_tables = cursor.fetchall()
-        for table in list_of_all_occupied_tables:
-            table = (table[0],table[1],table[2],table[3]-1)
-            if table[3]==0:
-                cursor.execute("DELETE FROM courses WHERE id = (?)", table[2])
-                cursor.execute("UPDATE classrooms SET current_course_id = 0 , current_course_time = 0 WHERE classrooms.id = (?)", table[0])
+        cursor.execute("SELECT * FROM classrooms ")
+        list_of_all_classrooms = cursor.fetchall()
+        for var in list_of_all_classrooms:
+            if var[3] == 0:
+                cursor.execute("SELECT * FROM courses WHERE class_id = (?) ORDER BY id ASC LIMIT 1", (var[0], ))
+                enter_to_classroomTable = cursor.fetchone()
+                if enter_to_classroomTable is not None and len(enter_to_classroomTable) != 0:
+                    print("("+str(number_of_iteration)+") " + str(var[1]) + ": " + str(enter_to_classroomTable[1]) + " is schedule to start")
+                    cursor.execute("UPDATE classrooms SET current_course_id = ? , current_course_time_left = ? WHERE classrooms.id = ?", [enter_to_classroomTable[0], enter_to_classroomTable[5], enter_to_classroomTable[4]])
+                    cursor.execute("SELECT count FROM students WHERE grade = ?", [enter_to_classroomTable[2]])
+                    count = cursor.fetchone()
+                    count= count[0]- enter_to_classroomTable[3]
+                    if count <= 0:
+                        count = 0
+                    cursor.execute("UPDATE students SET count = ? WHERE grade = ?", [count, enter_to_classroomTable[2]])
+                    dbcon.commit()
+            else:
+                cursor.execute("SELECT course_name FROM courses WHERE id = ?", [var[2]])
+                courseName = cursor.fetchone()
+                if var[3]-1!=0:
+                    print("("+str(number_of_iteration)+") " + str(var[1]) + ": " + "occupied by " + str(courseName[0]))
+                    cursor.execute("UPDATE classrooms SET current_course_time_left = current_course_time_left -1 WHERE id = ?",
+                                   [var[0]])
+                    dbcon.commit()
+                else:
+                    print("("+str(number_of_iteration)+") " + str(var[1]) + ": " +  str(courseName[0]) + " is done")
+                    cursor.execute("DELETE FROM courses WHERE id = ?", [var[2]])
+                    cursor.execute("UPDATE classrooms SET current_course_time_left = 0 , current_course_id = 0 WHERE id = ?",
+                                   [var[0]])
+                    dbcon.commit()
+                    cursor.execute("SELECT * FROM courses WHERE class_id = ? ORDER BY id ASC LIMIT 1", [var[0]])
+                    enter_to_classroomTable = cursor.fetchone()
 
-
-
-        cursor = dbcon.cursor()
-        cursor.execute("SELECT id FROM classrooms WHERE current_course_time_left = 0")
-        list_of_all_available_tables = cursor.fetchall()
-        for var in list_of_all_available_tables:
-            cursor.execute("SELECT TOP 1 * FROM courses WHERE classroom_id = (?) ORDER BY id ASC",var[0])
-            enter_to_classroomTable = cursor.fetchone()
-            cursor.execute("UPDATE classrooms SET current_course_id = (?) , current_course_time_left = (?) WHERE classrooms.id = (?)", [(enter_to_classroomTable[0],), (enter_to_classroomTable[5],), (enter_to_classroomTable[4],)])
-            cursor.execute("SELECT count FROM students WHERE grade = (?)", enter_to_classroomTable[2])
-            count = cursor.fetchone() - enter_to_classroomTable[3]
-            if count <= 0:
-                count = 0
-            cursor.execute("UPDATE students SET count = (?) WHERE grade = (?)", [(count,), (enter_to_classroomTable[2])])
-
+                    if enter_to_classroomTable is not None and len(enter_to_classroomTable) != 0:
+                        print("(" + str(number_of_iteration) + ") " + str(var[1]) + ": " + str(
+                            enter_to_classroomTable[1]) + " is schedule to start")
+                        cursor.execute(
+                            "UPDATE classrooms SET current_course_id = ? , current_course_time_left = ? WHERE classrooms.id = ?",
+                            [enter_to_classroomTable[0], enter_to_classroomTable[5],
+                             enter_to_classroomTable[4]])
+                        dbcon.commit()
+                        cursor.execute("SELECT count FROM students WHERE grade = (?)", (enter_to_classroomTable[2], ))
+                        count = cursor.fetchone()
+                        count = count[0] - enter_to_classroomTable[3]
+                        if count <= 0:
+                            count = 0
+                        cursor.execute("UPDATE students SET count = ? WHERE grade = ?",
+                                       [count, enter_to_classroomTable[2]])
+                        dbcon.commit()
+        number_of_iteration= number_of_iteration+1
+        printAllTables()
 
 
 
@@ -54,7 +103,7 @@ def check_if_table_is_empty():
         cursor = dbcon.cursor()
         cursor.execute("SELECT * FROM courses")
         coursesList = cursor.fetchall();
-        if len(coursesList.fetchall())==0:
+        if len(coursesList)==0:
             return True
         else:
             return False
